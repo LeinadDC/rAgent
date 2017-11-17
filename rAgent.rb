c40bacd5143ac73a62bf3463607a4a806bfb2570
Daniel <daniel.castellanom@gmail.com>
2017-11-16 18:01:58 -0800

    Hilo de inserción de datos y de revisión de espacios completado

--- rAgent.rb
+++ rAgent.rb
@@ -3,20 +3,38 @@ require 'pg'
 require 'net/ssh'
 require 'sys/filesystem'
 
-conn = PG::Connection.new("dbname=climate port=5432 host=192.168.98.129 user=postgres password=psql123" )
+conn = PG::Connection.new("dbname=climate port=5432 host=192.168.74.128 user=postgres password=psql123" )
 #Hilo que se encarga de insertar datos en la base de datos.
-postgresThread = Thread.new do
+
+queryThread_stopped = true
+
+queryThread = Thread.new do
   while true do
+    Thread.stop if queryThread_stopped
     url = 'http://api.openweathermap.org/data/2.5/weather?q=Heredia,cr&appid=a6625d60bbdd040cdb8daebce9f39ce0'
     response = HTTParty.get(url)
     conn.exec("INSERT INTO weather (data) values ('#{response}')")
+    #conn.exec("COPY weather to '/hdd/rubycopy.csv with csv'")
     timeElapsed = Time.now
     puts timeElapsed
-    sleep 6
+    sleep 2
   end
 end
 
-postgresThread.join
+revisionThread = Thread.new do
+  while true do
+    databaseSizeQuery = conn.exec("SELECT pg_size_pretty(pg_database_size('climate'));")
+    databaseSize= databaseSizeQuery.getvalue(0,0)
+    puts ("El tamaño de la tabla es " + databaseSize)
+    queryThread_stopped = false
+    queryThread.wakeup
+    sleep 2
+  end
+end
+
+revisionThread.join
+queryThread.join
+
 
 stat = Sys::Filesystem.stat("/")
 mb_available = stat.block_size * stat.blocks_available / 1024 / 1024
