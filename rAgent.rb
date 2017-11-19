5c760f0b48610e7fcc5d3a930a033b77129b2226
Daniel <daniel.castellanom@gmail.com>
2017-11-19 12:40:48 -0800

    Refactorizado el metodo princpial, terminando flujo

--- rAgent.rb
+++ rAgent.rb
@@ -3,49 +3,61 @@ require 'pg'
 require 'net/ssh'
 require 'sys/filesystem'
 
-conn = PG::Connection.new("dbname=climate port=5432 host=192.168.74.128 user=postgres password=psql123" )
+conn = PG::Connection.new("dbname=climate port=5432 host=192.168.98.129 user=postgres password=psql123" )
+
+def fillDatabase(conn)
+  url = 'http://api.openweathermap.org/data/2.5/weather?q=Heredia,cr&appid=a6625d60bbdd040cdb8daebce9f39ce0'
+  response = HTTParty.get(url)
+  #Ya que los datos no pesan mucho hacemos insert 300 veces por cada request para así lograr inflar la base de datos.
+  i = 0
+  while i < 300 do
+    conn.exec("INSERT INTO weather (data) values ('#{response}')")
+    i += 1
+  end
+end
+
+def getPartitionSpace
+  spaceMb_i = `df -m /dev/sda1`.split(/\b/)[24].to_i
+  puts spaceMb_i
+end
+
+def getDatabaseSize(conn)
+  databaseSizeQuery = conn.exec("SELECT pg_size_pretty(pg_database_size('climate'));")
+  databaseSize= databaseSizeQuery.getvalue(0, 0)
+  #Se pasa este dato a Integer y se le extraen los valores que no sean numericos
+  numberSize = Integer(databaseSize.gsub(/[^0-9]/, ''))
+  puts numberSize
+  numberSize
+end
+
+def clearDBSpace
+  #Si la base de datos está por encima de la capacidad indicada entonces se ejecuta el COPY
+  #Se debe cambiar el nombre de cada copia para así envitar que le caiga encima a la existente
+  #conn.exec("COPY weather to '/hdd/rubycopy.csv with csv'")
+  #Ejecuta COPY y espera a que termine de generar el CSV
+  #LIMPIA LA TABLA PARA NO TENER DATOS Y SEGUIR INSERTANDO
+  puts "Es necesario limpiar la base de datos - Iniciando proceso"
+end
+
 #Hilo que se encarga de insertar datos en la base de datos.
-
-queryThread_stopped = true
-
 queryThread = Thread.new do
   while true do
-    Thread.stop if queryThread_stopped
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=Heredia,cr&appid=a6625d60bbdd040cdb8daebce9f39ce0'
-    response = HTTParty.get(url)
-    conn.exec("INSERT INTO weather (data) values ('#{response}')")
-    #conn.exec("COPY weather to '/hdd/rubycopy.csv with csv'")
+    #Request al API del clima, obteniendo los datos de Heredia.
+    fillDatabase(conn)
+    #Se obtiene el tamaño de la base de datos que se está utilizando.
+    numberSize = getDatabaseSize(conn)
+    #Se obtiene el tamaño de la partición actual en la que se encuentra la base de datos
+    getPartitionSpace
+
+    #Se revisa si el tamaño actual de la partición es optimo para seguir recibiendo datos
+    if numberSize < 3000
+      clearDBSpace
+    end
+
     timeElapsed = Time.now
     puts timeElapsed
-    sleep 2
+    sleep 1
   end
 end
 
-revisionThread = Thread.new do
-  while true do
-    databaseSizeQuery = conn.exec("SELECT pg_size_pretty(pg_database_size('climate'));")
-    databaseSize= databaseSizeQuery.getvalue(0,0)
-    puts ("El tamaño de la tabla es " + databaseSize)
-    queryThread_stopped = false
-    queryThread.wakeup
-    sleep 2
-  end
-end
-
-revisionThread.join
 queryThread.join
-
-
-stat = Sys::Filesystem.stat("/")
-mb_available = stat.block_size * stat.blocks_available / 1024 / 1024
-
-puts "Espacio disponible: #{mb_available}"
-#if mb_available < 3500
- # puts "Es necesario levantar otra máquina virtual"
-  #puts "Iniciando proceso de clonación de máquina virtual"
-  #Clonación de máquina virtual
-  #puts "Encendiendo máquina virtual"
-  #Encender máquina virtual
-#else
- # puts "Esta máquina virtual todavía puede seguir recibiendo datos"
-#end
